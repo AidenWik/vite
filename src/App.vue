41475034f3ffc4e5d2b81582c1e7ffa062127a32
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-09-18 16:03:51 -0400

    updated with card

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,295 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+ <head>
+  <title>Homework IST 256 Sheetz</title>
+</head>
+<body>
+ <div class="card-container">
+  <div class="card-wrapper">
+  <div class="Sheetz">
+    <label class="sheetzLbl">Sheetz</label>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+<div class="directions">
+  <label id="directionsLbl">Directions</label>
+</div>
+  
+<div class="Favorites">
+  <label id="Favoriteslbl">Favorites</label>
+  </div>
+  
+<div class="btn-wrapper">
+  <button>Hotdog</button>
+  <button>Fries</button>
+  <button>Shake</button>
+</div>
+
+<div class="images">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png" alt="A beautiful landscape">
+
+  <img src="https://media.istockphoto.com/id/496094410/photo/potatoes-fries-in-a-red-carton-box-isolated-fast-food.jpg?s=612x612&w=0&k=20&c=d7WnO6AW0C3A-xMSBa3Yb5rmSNelyY-jmxhW2efZq2Q=">
+
+  <img 
+src="https://gofericecream.com/wp-content/uploads/2021/06/shake-.png"> 
+  
+</div>
+
+ <div class="detailsBtn">
+        <button class="toggle-details">Details</button>
+    </div>
+     
+  
+<div class="paragraphs">
+  <p1>$1.99 <br>A food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun.</p1>
+  <p2>$0.99 <br>Side dish or snack typically made from deep-fried potatoes that have been cut into various shapes, especially thin strips.</p2>
+  <p3>$5.99 <br> A sweet beverage made by blending milk, ice cream, and flavorings or sweeteners.</p3>
+  </div>
+
+ 
+  </div>
+
+  </div>
+  
+  <div class ="duper">
+   <button id="duper">Button for duplication</button>
+    </div>   
+  <script>
+    // JavaScript to toggle visibility of the paragraph descriptions
+    const toggleDetailsButtons = document.querySelectorAll('.toggle-details');
+    const paragraphsSections = document.querySelectorAll('.paragraphs');
+
+    toggleDetailsButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        paragraphsSections[index].classList.toggle('hidden');
+      });
+    });
+  </script>
+
+  <script>
+    // JavaScript for cloning cards
+    document.querySelector('#duper').addEventListener('click', (e) => {
+      const cardContainer = document.querySelector('.card-container');
+      const cardWrapperClone = document.querySelector('.card-wrapper').cloneNode(true);
+
+      // Add unique classes to the cloned elements
+      cardWrapperClone.classList.add('cloned-card');
+      cardWrapperClone.querySelector('.toggle-details').textContent = 'Details';
+
+      cardContainer.appendChild(cardWrapperClone);
+
+      // Set up event listeners for the cloned button
+      const clonedToggleDetailsButton = cardWrapperClone.querySelector('.toggle-details');
+      const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
+
+      clonedToggleDetailsButton.addEventListener('click', () => {
+        clonedParagraphsSection.classList.toggle('hidden');
+      });
+    });
+  </script>
+  
+   
+  
+  <div class ="background">
+    <button id="background">Change Background</button>
+  </div>
+   <script> document.querySelector('#background').addEventListener('click', (e) => {
+    const cardWrappers = document.querySelectorAll('.card-wrapper');
+    /* loop to change each existing iteration*/
+    cardWrappers.forEach((cardWrapper) => {
+        cardWrapper.classList.toggle('alt-bg');
+    });
+});
+
+</script>
+  
+  <div class ="title">
+    <button id="title">Change Title</button>
+  </div>
+  
+  <script>
+   document.querySelector('#title').addEventListener('click', (e) => {
+  const headings = document.querySelectorAll('.sheetzLbl'); // Corrected class name
+  headings.forEach((heading) => {
+    heading.textContent = "something else";
+  });
+});
+
+
+
+  </script>
+  
+    <div class="delete">
+        <button id="deleteLastCard">Delete Last Card</button>
+    </div>
+    <script> document.querySelector('#deleteLastCard').addEventListener('click', (e) => {
+            // Code for deleting the last card
+            const cardContainer = document.querySelector('.card-container');
+            const cardWrappers = document.querySelectorAll('.card-wrapper');
+            
+            // Check if there's at least one card to delete
+            if (cardWrappers.length > 1) {
+                const lastCard = cardWrappers[cardWrappers.length - 1];
+                cardContainer.removeChild(lastCard);
+            }
+        });
+    </script>
+
+  <script>
+  document.querySelector('.card-wrapper').addEventListener('mouseover', (e) => {
+    const cardWrapper = document.querySelector('.card-wrapper');
+        cardWrapper.classList.toggle('alt-bg2');
+});
+  </script>
+  
+  <script>
+  document.querySelector('.card-wrapper').addEventListener('mouseleave', (e) => {
+    const cardWrapper = document.querySelector('.card-wrapper');
+        cardWrapper.classList.toggle('alt-bg2');
+});
+  </script>
+   
+
+
+  </body>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+.card-wrapper {
+  display:inline-block;
+  max-width: 400px; /* Set the maximum width of the card */
+  width: 100%; /* Make the card responsive within the parent container */
+  padding: 20px; /* Add padding for spacing */
+  border: 1px solid #ccc;
+  text-align: center;
+  position: relative;
+  margin-bottom: 200px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.card-wrapper img {
+  width: 100%; /* Set the width of the image to 100% to fit the card width */
+  max-height: 300px; /* Set a fixed maximum height for the image */
+  object-fit: cover; /* Maintain aspect ratio and cover the entire image area */
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+/* Media query to scale the card on screens smaller than 500px */
+@media (max-width: 500px) {
+  .card-wrapper {
+      max-width: 100%; /* Expand the card to the full width of the viewport */
+      padding: 10px; /* Adjust padding for smaller screens if needed */
+  }
 }
+
+.Sheetz label{ 
+
+display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh; 
+  margin: 0; 
+font-size: 50px;
+color: white;
+background: red;
+}
+.directions label{
+display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh; 
+  margin: 0;
+color: Black;
+}
+.Favorites label{
+display: flex;
+  justify-content: center;
+  align-items: center; 
+  margin: 0;
+font-size: 30px;
+color: white;
+background: black;
+}
+
+.btn-wrapper {
+display: flex;
+  justify-content: center;
+  align-items: center; 
+margin: 24px auto;
+}
+.btn-wrapper button {
+font-size: 40px;
+color: blue;
+background-color: white;
+}
+.btn-wrapper button:hover {
+color: white;
+background-color: blue;
+}
+.images {
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.images img {
+height: auto; 
+max-width: 30%; 
+}
+
+.paragraphs {
+display: flex;
+  justify-content: center;
+  align-items: center; 
+}
+.paragraphs p1 {
+/* Styles for the first paragraph */
+padding-right: 20px;
+padding-bottom: 20px;
+}
+.paragraphs p2 {
+padding-right: 20px;
+padding-bottom: 20px;
+}
+.paragraphs p3 {
+padding-right: 20px;
+padding-bottom: 20px;
+}
+.card-wrapper .detailsBtn button {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  font-size: 14px;
+  padding: 8px 16px;
+  background-color: blue;
+  cursor: pointer;
+  color: white;
+}
+
+.detailsBtn button:hover {
+  color: blue;
+  background-color: white;
+}
+
+.duper button{
+color: white;
+background-color: blue;
+}
+.duper button:focus  {
+color: blue;
+  background-color: white;
+}
+/* Show the details button between 500px and 800px */
+@media (max-width: 800px) {
+  .detailsBtn button {
+      display: none;
+  }
+}
+
+.card-wrapper.alt-bg {
+  background-color: lightblue;
+}
+.card-wrapper.alt-bg2 {
+  background-color: lightgreen;
+}
+.hidden {
+          display: none;
+      }
 </style>
